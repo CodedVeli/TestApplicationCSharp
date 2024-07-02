b7bb65c4a21fee55a40f10c8b5d220f21c99d85c
MoriasiMa <129747456+MoriasiMa@users.noreply.github.com>
2024-07-02 17:04:01 +0300

    Add files via upload

---
+++ WebApplication1/Models/IndexModel.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication1.Controllers;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class IndexModel
+    {
+        public List<Person> Persons { get; set; }
+        public string? SelectedValue { get; set; }
+        public Person? SelectedItem { get; set; }
+    }
+
+    public class Person
+    {
+        public string? Value { get; set; }
+        public string? Name { get; set; }
+    }
+}
